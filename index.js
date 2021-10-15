efff35b9cfbb1cb21203a976ec4d23b34fa02285
Ngina1234 <85606157+Ngina1234@users.noreply.github.com>
2021-10-15 16:49:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function calculator(num1, num2) {
+  let num1 = 200;
+  let num2 = 30;
+
+  const sum = num1 + num2;
+  const diff = num1 - num2;
+  const product = num1 * num2;
+  const quot = num1 / num2;
+
+  console.log("sum: " + sum);
+  console.log("difference: " + diff);
+  console.log("product: " + product);
+  console.log("quotient: " + quot);
+}
+calculator(200, 56);
