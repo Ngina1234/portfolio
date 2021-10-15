7d39b59f4608b7021959f5a10580111e009e002b
Ngina1234 <85606157+Ngina1234@users.noreply.github.com>
2021-10-15 16:51:11 +0300

    JS

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function calculator(num1, num2) {
-  let num1 = 200;
-  let num2 = 30;
+  let num1 = document.getElementById("num1");
+  let num2 = document.getElementById("num2");
 
   const sum = num1 + num2;
   const diff = num1 - num2;
   const product = num1 * num2;
   const quot = num1 / num2;
 
-  console.log("sum: " + sum);
-  console.log("difference: " + diff);
-  console.log("product: " + product);
-  console.log("quotient: " + quot);
+  //console.log("Sum: + sum");
+  //console.log("difference: + diff");
+  //console.log("Product: + Product");
+  //console.log("quotientt: + quot");
 }
 calculator(200, 56);
