3751f04bc87af1d7ab5e68a5f0b1db0b888ef3d9
Ngina1234 <85606157+Ngina1234@users.noreply.github.com>
2021-10-22 16:56:24 +0300

    j.s

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 function calculator(num1, num2) {
   let num1 = document.getElementById("num1");
   let num2 = document.getElementById("num2");
+  let output =document.getElementById("output");
 
-  const sum = num1 + num2;
-  const diff = num1 - num2;
-  const product = num1 * num2;
-  const quot = num1 / num2;
+  num1 = parseInt(num1.value);
+  num2 = parseInt(num2.value);
 
-  //console.log("Sum: + sum");
-  //console.log("difference: + diff");
-  //console.log("Product: + Product");
-  //console.log("quotientt: + quot");
+  const sum=num1+num2;
+  const diff =num1-num1;
+  const product =num1*num2;
+  const quot= num1/num2;
+  output.innerHTML ="+sum+"\n difference 
+
+  console.log("Sum: + sum");
+  console.log("difference: + diff");
+  console.log("Product: + Product");
+console.log("quotientt: + quot");
+console.log("type of quotient:"=typeof quot);
 }
-calculator(200, 56);
+let navbar_icon = document.getElementById ("navbar-icon");
+navbar_icon.addEventListener("click",function () {
+  let collapse =document.getElementById("collapse");
+  if(collapse.style.diplay=="none")
+  collapse.style.display="flex";
+  else
+  collapse.style.display = "none";
+});
+
+
